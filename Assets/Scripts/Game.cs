b9555ddfe61d7f69029475f831216fff61a7bad1
BlackTurkey <justin.aaa99@gmail.com>
2021-12-20 00:01:15 +0800

    Add difficulty

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -3,57 +3,19 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+public enum DifficultyType : ushort { Start, Easy, Normal, Hard };
+
 public class Game : MonoBehaviour {
-    public enum DifficultyType : ushort { Start, Easy, Normal, Hard };
 
     public Character[] Players;
 
     private DifficultyType Difficulty;
 
-    private float[,] Location = new float[12, 2] {
-        {-216, 41},
-        {-240, 159},
-        {11, -53},
-        {-91, 159},
-        {4, 96},
-        {-137, 245},
-        {184, 25},
-        {-4, 205},
-        {98, 158},
-        {-7, 287},
-        {227, 158},
-        {122, 247}
-    };
+    private float[,] Location;
 
-    private int[,] Neighbor = new int[12, 4] { // 0:紅順, 1:紅逆, 2:藍順, 3:藍逆 
-        {1, 2, 1, 3},
-        {5, 0, 3, 0},
-        {0, 6, 4, 6},
-        {7, 4, 0, 1},
-        {3, 8, 6, 2},
-        {9, 1, 9, 7},
-        {2, 10, 2, 4},
-        {8, 3, 5, 9},
-        {4, 7, 11, 10},
-        {11, 5, 7, 5},
-        {6, 11, 8, 11},
-        {10, 9, 10, 8}
-    };
+    private int[,] Neighbor;
 
-    private int[,] Distance = new int[12, 12] { 
-        {0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4},
-        {1, 0, 2, 1, 2, 1, 3, 2, 3, 2, 4, 3},
-        {1, 2, 0, 2, 1, 3, 1, 3, 2, 4, 2, 3},
-        {1, 1, 2, 0, 1, 2, 2, 1, 2, 2, 3, 3},
-        {2, 2, 1, 1, 0, 3, 1, 2, 1, 3, 2, 2},
-        {2, 1, 3, 2, 3, 0, 4, 1, 2, 1, 3, 2},
-        {2, 3, 1, 2, 1, 4, 0, 3, 2, 3, 1, 2},
-        {2, 2, 3, 1, 2, 1, 3, 0, 1, 1, 2, 2},
-        {3, 3, 2, 2, 1, 2, 2, 1, 0, 2, 1, 1},
-        {3, 2, 4, 2, 3, 1, 3, 1, 2, 0, 2, 1},
-        {3, 4, 2, 3, 2, 3, 1, 2, 1, 2, 0, 1},
-        {4, 3, 3, 3, 2, 2, 2, 2, 1, 1, 1, 0}
-    };
+    private int[,] Distance;
 
     private ushort Round;
     private bool IsGuest;
@@ -66,6 +28,9 @@ public class Game : MonoBehaviour {
     private float time;
     private ushort PurchaseCount;
 
+    [SerializeField] private Sprite[] Skin;
+    [SerializeField] private Sprite[] Map;
+    [SerializeField] private GameObject BattleGround;
     [SerializeField] private GameObject Character1;
     [SerializeField] private GameObject Character2;
     [SerializeField] private GameObject[] Code_Area;
@@ -81,7 +46,10 @@ public class Game : MonoBehaviour {
     [SerializeField] private GameObject PrepareTime;
     [SerializeField] private GameObject CoverField;
     [SerializeField] private GameObject Purchase;
-    [SerializeField] private GameObject Instruction_Move;
+    [SerializeField] private GameObject[] Commodity;
+    [SerializeField] private GameObject Instruction_Move_SE;
+    [SerializeField] private GameObject Instruction_Move_N;
+    [SerializeField] private GameObject Instruction_Move_H;
     [SerializeField] private GameObject Instruction_Attack;
     [SerializeField] private GameObject Instruction_Assign;
     [SerializeField] private GameObject Instruction_If;
@@ -90,23 +58,273 @@ public class Game : MonoBehaviour {
 
     void Awake() {
         //IsGuest = "" 傳入是否為被接受對戰者 ""
-        //暫定
         Players = new Character[2];
-        Players[0] = new Character(CharacterType.Fox);
-        Players[1] = new Character(CharacterType.Kangaroo);
-        Difficulty = DifficultyType.Hard;
+        CharacterType[] characterType = new CharacterType[2];
+        // "" 取得角色資訊與難度 ""
+        characterType[0] = CharacterType.Whale; //暫定
+        characterType[1] = CharacterType.Kangaroo; //暫定
+        Difficulty = DifficultyType.Hard; //暫定
+        Players[0] = new Character(characterType[0]);
+        Character1.GetComponent<Image>().sprite = Skin[(int)characterType[0]];
+        Players[1] = new Character(characterType[1]);
+        Character2.GetComponent<Image>().sprite = Skin[(int)characterType[1]];
+        BattleGround.GetComponent<Image>().sprite = Map[(int)Difficulty];
+        switch (Difficulty) {
+            case DifficultyType.Start:
+                Commodity[0].SetActive(true);
+                Commodity[1].SetActive(false);
+                Commodity[2].SetActive(false);
+                Commodity[3].SetActive(true);
+                Commodity[4].SetActive(false);
+                Commodity[5].SetActive(false);
+                Commodity[6].SetActive(false);
+                Commodity[7].SetActive(false);
+                Commodity[8].SetActive(false);
+                Location = new float[16, 2] {
+                    {-149, 277},
+                    {-52, 277},
+                    {48, 277},
+                    {134, 277},
+                    {-166, 194},
+                    {-57, 194},
+                    {46, 194},
+                    {155, 194},
+                    {-191, 96},
+                    {-65, 96},
+                    {55, 96},
+                    {172, 96},
+                    {-200, -21},
+                    {-80, -21},
+                    {52, -21},
+                    {184, -21}
+                };
+                Neighbor = new int[16, 4] { // 0:上, 1:下, 2:左, 3:右 
+                    {0, 4, 0, 1},
+                    {1, 5, 0, 2},
+                    {2, 6, 1, 3},
+                    {3, 7, 2, 3},
+                    {0, 8, 4, 5},
+                    {1, 9, 4, 6},
+                    {2, 10, 5, 7},
+                    {3, 11, 6, 7},
+                    {4, 12, 8, 9},
+                    {5, 13, 8, 10},
+                    {6, 14, 9, 11},
+                    {7, 15, 10, 11},
+                    {8, 12, 12, 13},
+                    {9, 13, 12, 14},
+                    {10, 14, 13, 15},
+                    {11, 15, 14, 15}
+                };
+                Distance = new int[16, 16] {
+                    {0, 1, 2, 3, 1, 2, 3, 4, 2, 3, 4, 5, 3, 4, 5, 6},
+                    {1, 0, 1, 2, 2, 1, 2, 3, 3, 2, 3, 4, 4, 3, 4, 5},
+                    {2, 1, 0, 1, 3, 2, 1, 2, 4, 3, 2, 3, 5, 4, 3, 4},
+                    {3, 2, 1, 0, 4, 3, 2, 1, 5, 4, 3, 2, 6, 5, 4, 3},
+                    {1, 2, 3, 4, 0, 1, 2, 3, 1, 2, 3, 4, 2, 3, 4, 5},
+                    {2, 1, 2, 3, 1, 0, 1, 2, 2, 1, 2, 3, 3, 2, 3, 4},
+                    {3, 2, 1, 2, 2, 1, 0, 1, 3, 2, 1, 2, 4, 3, 2, 3},
+                    {4, 3, 2, 1, 3, 2, 1, 0, 4, 3, 2, 1, 5, 4, 3, 2},
+                    {2, 3, 4, 5, 1, 2, 3, 4, 0, 1, 2, 3, 1, 2, 3, 4},
+                    {3, 2, 3, 4, 2, 1, 2, 3, 1, 0, 1, 2, 2, 1, 2, 3},
+                    {4, 3, 2, 3, 3, 2, 1, 2, 2, 1, 0, 1, 3, 2, 1, 2},
+                    {5, 4, 3, 2, 4, 3, 2, 1, 3, 2, 1, 0, 4, 3, 2, 1},
+                    {3, 4, 5, 6, 2, 3, 4, 5, 1, 2, 3, 4, 0, 1, 2, 3},
+                    {4, 3, 4, 5, 3, 2, 3, 4, 2, 1, 2, 3, 1, 0, 1, 2},
+                    {5, 4, 3, 4, 4, 3, 2, 3, 3, 2, 1, 2, 2, 1, 0, 1},
+                    {6, 5, 4, 3, 5, 4, 3, 2, 4, 3, 2, 1, 3, 2, 1, 0}
+                };
+                if (!IsGuest) {
+                    Players[0].Pos = 12;
+                    Players[1].Pos = 3;
+                }
+                else {
+                    Players[0].Pos = 3;
+                    Players[1].Pos = 12;
+                }
+                break;
+            case DifficultyType.Easy:
+                Commodity[0].SetActive(true);
+                Commodity[1].SetActive(false);
+                Commodity[2].SetActive(false);
+                Commodity[3].SetActive(true);
+                Commodity[4].SetActive(true);
+                Commodity[5].SetActive(false);
+                Commodity[6].SetActive(true);
+                Commodity[7].SetActive(false);
+                Commodity[8].SetActive(false);
+                Location = new float[16, 2] {
+                    {-141, 253},
+                    {-42, 253},
+                    {60, 253},
+                    {153, 253},
+                    {-158, 180},
+                    {-56, 180},
+                    {46, 180},
+                    {151, 180},
+                    {-174, 104},
+                    {-72, 104},
+                    {41, 104},
+                    {154, 104},
+                    {-211, 14},
+                    {-92, 14},
+                    {41, 14},
+                    {157, 14}
+                };
+                Neighbor = new int[16, 4] { // 0:上, 1:下, 2:左, 3:右 
+                    {12, 4, 3, 1},
+                    {13, 5, 0, 2},
+                    {14, 6, 1, 3},
+                    {15, 7, 2, 0},
+                    {0, 8, 7, 5},
+                    {1, 9, 4, 6},
+                    {2, 10, 5, 7},
+                    {3, 11, 6, 4},
+                    {4, 12, 11, 9},
+                    {5, 13, 8, 10},
+                    {6, 14, 9, 11},
+                    {7, 15, 10, 8},
+                    {8, 0, 15, 13},
+                    {9, 1, 12, 14},
+                    {10, 2, 13, 15},
+                    {11, 3, 14, 12}
+                };
+                Distance = new int[16, 16] {
+                    {0, 1, 2, 1, 1, 2, 3, 2, 2, 3, 4, 3, 1, 2, 3, 4},
+                    {1, 0, 1, 2, 2, 1, 2, 3, 3, 2, 3, 4, 2, 1, 2, 3},
+                    {2, 1, 0, 1, 3, 2, 1, 2, 4, 3, 2, 3, 3, 2, 1, 2},
+                    {1, 2, 1, 0, 2, 3, 2, 1, 3, 4, 3, 2, 4, 3, 2, 1},
+                    {1, 2, 3, 2, 0, 1, 2, 1, 1, 2, 3, 2, 2, 3, 4, 3},
+                    {2, 1, 2, 3, 1, 0, 1, 2, 2, 1, 2, 3, 3, 2, 3, 4},
+                    {3, 2, 1, 2, 2, 1, 0, 1, 3, 2, 1, 2, 4, 3, 2, 3},
+                    {2, 3, 2, 1, 1, 2, 1, 0, 2, 3, 2, 1, 3, 4, 3, 2},
+                    {2, 3, 4, 3, 1, 2, 3, 2, 0, 1, 2, 1, 1, 2, 3, 2},
+                    {3, 2, 3, 4, 2, 1, 2, 3, 1, 0, 1, 2, 2, 1, 2, 3},
+                    {4, 3, 2, 3, 3, 2, 1, 2, 2, 1, 0, 1, 3, 2, 1, 2},
+                    {3, 4, 3, 2, 2, 3, 2, 1, 1, 2, 1, 0, 2, 3, 2, 1},
+                    {1, 2, 3, 4, 2, 3, 4, 3, 1, 2, 3, 2, 0, 1, 2, 1},
+                    {2, 1, 2, 3, 3, 2, 3, 4, 2, 1, 2, 3, 1, 0, 1, 2},
+                    {3, 2, 1, 2, 4, 3, 2, 3, 3, 2, 1, 2, 2, 1, 0, 1},
+                    {4, 3, 2, 1, 3, 4, 3, 2, 2, 3, 2, 1, 1, 2, 1, 0}
+                };
+                if (!IsGuest) {
+                    Players[0].Pos = 12;
+                    Players[1].Pos = 3;
+                }
+                else {
+                    Players[0].Pos = 3;
+                    Players[1].Pos = 12;
+                }
+                break;
+            case DifficultyType.Normal:
+                Commodity[0].SetActive(false);
+                Commodity[1].SetActive(true);
+                Commodity[2].SetActive(false);
+                Commodity[3].SetActive(true);
+                Commodity[4].SetActive(true);
+                Commodity[5].SetActive(false);
+                Commodity[6].SetActive(true);
+                Commodity[7].SetActive(true);
+                Commodity[8].SetActive(false);
+                Location = new float[6, 2] {
+                    {-3, 284},
+                    {-102, 185},
+                    {99, 185},
+                    {-244, 52},
+                    {7, 54},
+                    {251, 46},
+                };
+                Neighbor = new int[6, 4] { // 0:紅, 1:黃, 2:綠, 3:藍 
+                    {1, 2, 5, 3},
+                    {0, 4, 3, 2},
+                    {5, 0, 4, 1},
+                    {4, 5, 1, 0},
+                    {3, 1, 2, 5},
+                    {2, 3, 0, 4},
+                };
+                Distance = new int[6, 6] {
+                    {0, 1, 1, 1, 2, 1},
+                    {1, 0, 1, 1, 1, 2},
+                    {1, 1, 0, 2, 1, 1},
+                    {1, 1, 2, 0, 1, 1},
+                    {2, 1, 1, 1, 0, 1},
+                    {1, 2, 1, 1, 1, 0}
+                };
+                if (!IsGuest) {
+                    Players[0].Pos = 4;
+                    Players[1].Pos = 0;
+                }
+                else {
+                    Players[0].Pos = 0;
+                    Players[1].Pos = 4;
+                }
+                break;
+            case DifficultyType.Hard:
+                Commodity[0].SetActive(false);
+                Commodity[1].SetActive(false);
+                Commodity[2].SetActive(true);
+                Commodity[3].SetActive(true);
+                Commodity[4].SetActive(false);
+                Commodity[5].SetActive(true);
+                Commodity[6].SetActive(true);
+                Commodity[7].SetActive(true);
+                Commodity[8].SetActive(true);
+                Location = new float[12, 2] {
+                    {-216, 41},
+                    {-240, 159},
+                    {11, -53},
+                    {-91, 159},
+                    {4, 96},
+                    {-137, 245},
+                    {184, 25},
+                    {-4, 205},
+                    {98, 158},
+                    {-7, 287},
+                    {227, 158},
+                    {122, 247}
+                };
+                Neighbor = new int[12, 4] { // 0:紅順, 1:紅逆, 2:藍順, 3:藍逆 
+                    {1, 2, 1, 3},
+                    {5, 0, 3, 0},
+                    {0, 6, 4, 6},
+                    {7, 4, 0, 1},
+                    {3, 8, 6, 2},
+                    {9, 1, 9, 7},
+                    {2, 10, 2, 4},
+                    {8, 3, 5, 9},
+                    {4, 7, 11, 10},
+                    {11, 5, 7, 5},
+                    {6, 11, 8, 11},
+                    {10, 9, 10, 8}
+                };
+                Distance = new int[12, 12] {
+                    {0, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 4},
+                    {1, 0, 2, 1, 2, 1, 3, 2, 3, 2, 4, 3},
+                    {1, 2, 0, 2, 1, 3, 1, 3, 2, 4, 2, 3},
+                    {1, 1, 2, 0, 1, 2, 2, 1, 2, 2, 3, 3},
+                    {2, 2, 1, 1, 0, 3, 1, 2, 1, 3, 2, 2},
+                    {2, 1, 3, 2, 3, 0, 4, 1, 2, 1, 3, 2},
+                    {2, 3, 1, 2, 1, 4, 0, 3, 2, 3, 1, 2},
+                    {2, 2, 3, 1, 2, 1, 3, 0, 1, 1, 2, 2},
+                    {3, 3, 2, 2, 1, 2, 2, 1, 0, 2, 1, 1},
+                    {3, 2, 4, 2, 3, 1, 3, 1, 2, 0, 2, 1},
+                    {3, 4, 2, 3, 2, 3, 1, 2, 1, 2, 0, 1},
+                    {4, 3, 3, 3, 2, 2, 2, 2, 1, 1, 1, 0}
+                };
+                if (!IsGuest) {
+                    Players[0].Pos = 0;
+                    Players[1].Pos = 11;
+                }
+                else {
+                    Players[0].Pos = 11;
+                    Players[1].Pos = 0;
+                }
+                break;
+        }
     }
 
     void Start() {
         Round = 1;
-        if (!IsGuest) {
-            Players[0].Pos = 0;
-            Players[1].Pos = 11;
-        }
-        else {
-            Players[0].Pos = 11;
-            Players[1].Pos = 0;
-        }
         UpdateHP();
         UpdateLocation();
         UpdateVariable();
@@ -182,10 +400,14 @@ public class Game : MonoBehaviour {
         PrepareTime.SetActive(false);
         CoverField.SetActive(true);
         Purchase.SetActive(false);
-        PlayerVariable.SetActive(true);
-        EnemyVariable.SetActive(true);
-        PlayerFood.SetActive(true);
-        EnemyFood.SetActive(true);
+        if (Difficulty != DifficultyType.Start) {
+            PlayerVariable.SetActive(true);
+            EnemyVariable.SetActive(true);
+        }
+        if (Difficulty == DifficultyType.Hard) {
+            PlayerFood.SetActive(true);
+            EnemyFood.SetActive(true);
+        }
         PlayerHP.SetActive(true);
         EnemyHP.SetActive(true);
         PlayerCode.transform.GetChild(2).gameObject.SetActive(true);
@@ -201,11 +423,17 @@ public class Game : MonoBehaviour {
         UpdateCost(true);
         UpdateVariable();
         if (!IsGuest) {
-            for (int i = 0; i < 10; i++) {
-                int number = Random.Range(1, 15);
-                Players[0].food[i] = number;
-                Players[1].food[i] = number;
-            }
+            if (Difficulty == DifficultyType.Hard)
+                for (int i = 0; i < 10; i++) {
+                    int number = Random.Range(1, 15);
+                    Players[0].food[i] = number;
+                    Players[1].food[i] = number;
+                }
+            else
+                for (int i = 0; i < 10; i++) {
+                    Players[0].food[i] = 0;
+                    Players[1].food[i] = 0;
+                }
         }
         else {
             // "" 取得food資料 ""
@@ -286,10 +514,12 @@ public class Game : MonoBehaviour {
                             Players[active].variable[target.Arguments[4]] = s1 * s2;
                             break;
                         case 3: // /
-                            Players[active].variable[target.Arguments[4]] = s1 / s2;
+                            if (s2 != 0)
+                                Players[active].variable[target.Arguments[4]] = s1 / s2;
                             break;
                         case 4: // %
-                            Players[active].variable[target.Arguments[4]] = s1 % s2;
+                            if (s2 != 0)
+                                Players[active].variable[target.Arguments[4]] = s1 % s2;
                             break;
                     }
                 }
@@ -382,7 +612,23 @@ public class Game : MonoBehaviour {
             Instruction target = Players[num].code[ProgramCounter];
             switch (target.Type) {
                 case InstructionType.Move:
-                    instruction = Instantiate(Instruction_Move);
+                    switch (Difficulty) {
+                        case DifficultyType.Start:
+                            instruction = Instantiate(Instruction_Move_SE);
+                            break;
+                        case DifficultyType.Easy:
+                            instruction = Instantiate(Instruction_Move_SE);
+                            break;
+                        case DifficultyType.Normal:
+                            instruction = Instantiate(Instruction_Move_N);
+                            break;
+                        case DifficultyType.Hard:
+                            instruction = Instantiate(Instruction_Move_H);
+                            break;
+                        default:
+                            instruction = Instantiate(Instruction_Move_SE);
+                            break;
+                    }
                     instruction.transform.GetChild(0).GetChild(2).GetComponent<Dropdown>().value = target.Arguments[0];
                     break;
                 case InstructionType.Attack:
