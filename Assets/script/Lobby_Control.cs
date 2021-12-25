23e5146e6d9c41dfdb19470faca191fd92c2f611
unknown <willy82486@gmail.com>
2021-12-26 05:12:06 +0800

    新增更新列表UI和實作，接受和送出挑戰UI和功能實作，大廳更改玩家名稱優化

--- Assets/script/Lobby_Control.cs
+++ Assets/script/Lobby_Control.cs
@@ -3,13 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEditor;
 
 public class Lobby_Control : MonoBehaviour
 {
+    [SerializeField] GameObject ReviseNameWindow;
+    [SerializeField] GameObject SingleMotionBtn;
+    [SerializeField] GameObject DoubleMotionBtn;
+    [SerializeField] GameObject StoreBtn;
+    [SerializeField] GameObject ReviseNameBtn;
+    [SerializeField] GameObject BookBtn;
+    [SerializeField] GameObject AchievementBtn;
+    [SerializeField] GameObject SettingBtn;
+    [SerializeField] GameObject WarningOnPlayerNameWindow;
+    [SerializeField] GameObject PlayerName;
+    [SerializeField] Text RevisePlayerName;
+    [SerializeField] GameObject RevisePlayerNamePlaceholder;
     // Start is called before the first frame update
     void Start()
     {
-        
+        RevisePlayerNamePlaceholder.GetComponent<Text>().text = PlayerName.GetComponent<Text>().text;
+        ReviseNameWindow.SetActive(false);
+        WarningOnPlayerNameWindow.SetActive(false);
     }
 
     // Update is called once per frame
@@ -43,7 +58,53 @@ public class Lobby_Control : MonoBehaviour
     }
     public void ReviseNameOnClick()
     {
+        ReviseNameWindow.SetActive(true);
+        SingleMotionBtn.SetActive(false);
+        DoubleMotionBtn.SetActive(false);
+        StoreBtn.SetActive(false);
+        ReviseNameBtn.SetActive(false);
+        BookBtn.SetActive(false);
+        AchievementBtn.SetActive(false);
+        SettingBtn.SetActive(false);
+    }
+    public void ReviseNameConfirm()
+    {
+        if (RevisePlayerName.text.Length > 9 || RevisePlayerName.text.Length == 0) {
+            WarningOnPlayerNameWindow.SetActive(true);
+        }
+        else {
+            PlayerName.GetComponent<Text>().text = RevisePlayerName.text;
+            RevisePlayerNamePlaceholder.GetComponent<Text>().text = PlayerName.GetComponent<Text>().text;
+            RevisePlayerName.text.Remove(0, PlayerName.GetComponent<Text>().text.Length);
+            ReviseNameWindow.SetActive(false);
+            SingleMotionBtn.SetActive(true);
+            DoubleMotionBtn.SetActive(true);
+            StoreBtn.SetActive(true);
+            ReviseNameBtn.SetActive(true);
+            BookBtn.SetActive(true);
+            AchievementBtn.SetActive(true);
+            SettingBtn.SetActive(true);
+        }
+    }
 
+    public void ReviseNameCancel()
+    {
+        RevisePlayerNamePlaceholder.GetComponent<Text>().text = PlayerName.GetComponent<Text>().text;
+        RevisePlayerName.text = "";
+        ReviseNameWindow.SetActive(false);
+        SingleMotionBtn.SetActive(true);
+        DoubleMotionBtn.SetActive(true);
+        StoreBtn.SetActive(true);
+        ReviseNameBtn.SetActive(true);
+        BookBtn.SetActive(true);
+        AchievementBtn.SetActive(true);
+        SettingBtn.SetActive(true);
+    }
+    public void WarningOnPlayerNameRealizeOnClick()
+    {
+        WarningOnPlayerNameWindow.SetActive(false);
+        RevisePlayerNamePlaceholder.GetComponent<Text>().text = PlayerName.GetComponent<Text>().text; ;
+        RevisePlayerName.text = "";
     }
     public void ExceptForLobbyReturnOnClick()
     {
