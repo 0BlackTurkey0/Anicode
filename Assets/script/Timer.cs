87ed0c25f9273d0ff5e11c1676e65455494121e1
unknown <wenwen510238@gmail.com>
2021-12-31 01:03:41 +0800

    簡單模式進入對戰加入時間條

---
+++ Assets/script/Timer.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    //public GameObject progress;
+    public Slider slider;
+    public Text progressText;
+    private int i = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(timer());
+    }
+
+    IEnumerator timer()
+    {
+        while (i < 99)
+        {
+            i += 1;
+            slider.value = i;
+            progressText.text = i.ToString()+" %";
+            yield return null;
+        }
+    }
+}
